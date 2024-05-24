7130e6f0833b761eee7c3e02ad92065f511de30d
Hilal-Bilun-Kara <bilunkara@gmail.com>
2024-05-24 14:58:26 +0300

    S13D3 Wall DONE

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,5 +11,18 @@ public class Main {
         System.out.println("Age: " + person.getAge());
 
 
+        System.out.println("******");
+
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
     }
 }
